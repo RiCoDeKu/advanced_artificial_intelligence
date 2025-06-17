60d4469f9df1cdbc0e91eaf1ff0000e46522b5b4
RiCoDeKu <mc.games.jumon@gmail.com>
2025-06-17 12:46:05 +0900

    installerの追加

---
+++ install_1.sh
@@ -0,0 +1,3 @@
+curl -LsSf https://astral.sh/uv/install.sh | sh
+source $HOME/.local/bin/env
+uv venv .nakamura --python=3.10
---
+++ install_2.sh
@@ -0,0 +1,8 @@
+uv pip install -r requirements.txt 
+/usr/local/cuda/bin/nvcc --version
+# 上記の文からCUDAのバージョンにあったPyTorchのバージョンをインストールするように指示
+echo "#############################################################################"
+echo ""
+echo "#  Please install the appropriate version of PyTorch for your CUDA version  #"
+echo ""
+echo "#############################################################################"
